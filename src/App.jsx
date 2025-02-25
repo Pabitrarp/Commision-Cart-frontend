06daa9dd61cb1ef775ede03de2539cc3b4b5a774
Pabitrarp <pabitramoharana5678@gmail.com>
2025-02-25 21:37:58 +0530

    finnaly

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import userrouter from "./component/user/userroutes";
 import adminroutes from "./component/admin/adminroutes";
 
 import { Homepage } from "./component/user/Homepage";
+import { Adminsignin } from "./component/admin/Adminsignin";
+import { Dashboard } from "./component/admin/Dashboard";
+import { Alluserlist } from "./component/admin/Alluserlist";
+import { Order } from "./component/user/Order";
 
 const App = () => {
   return (
@@ -13,10 +17,14 @@ const App = () => {
         <Route key={index} path={user.path} element={<user.Component />} />
       ))}
 
-    {adminroutes.map((admin, index) => (
+    {/* {adminroutes.map((admin, index) => (
       
         <Route key={index} path={admin.path} element={<admin.Component />} />
-      ))}
+      ))} */}
+       <Route path="/admin/login" element={<Adminsignin></Adminsignin>}/>
+       <Route path="/admin/dashboard" element={<Dashboard></Dashboard>}/>
+       <Route path="/admin/users" element={<Alluserlist></Alluserlist>}/>
+       <Route path="/admin/orders" element={<Order/>}/>
 
       <Route path="/" element={<Homepage/>}></Route>
     </Routes>
